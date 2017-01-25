7dd66dcb5603ddb393dc2df7b1fd52695003e0b8
jb2718 <jbattle2718@gmail.com>
2017-01-25 13:49:10 -0500

    converted tabs to spaces

--- cms.rb
+++ cms.rb
@@ -10,132 +10,132 @@ configure do
 end
 
 before do
-	@documents = []
-	if !load_file_data.empty?
-		@documents = load_file_data
-	end
+  @documents = []
+  if !load_file_data.empty?
+    @documents = load_file_data
+  end
 end
 
 def load_accounts
-	if ENV["RACK_ENV"] == "test"
-		path = File.expand_path("../test/test_user_accounts.yml",__FILE__)
-	else
-		path = File.expand_path("../user_accounts.yml",__FILE__)
-	end
-	YAML.load_file(path)
+  if ENV["RACK_ENV"] == "test"
+    path = File.expand_path("../test/test_user_accounts.yml",__FILE__)
+  else
+    path = File.expand_path("../user_accounts.yml",__FILE__)
+  end
+  YAML.load_file(path)
 end
 
 ACCOUNTS = load_accounts
 
 def data_path
-	if ENV["RACK_ENV"] == "test"
-		File.expand_path("../test/data",__FILE__)
-	else
-		File.expand_path("../data",__FILE__)
-	end
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data",__FILE__)
+  else
+    File.expand_path("../data",__FILE__)
+  end
 end
 
 def load_file_data
-	documents = []
-	file_pattern = File.join(data_path, "*")
-	Dir[file_pattern].each do |string|
-		file_name = File.basename(string)
-		path = File.dirname(string)
-		documents << Document.new
-		documents.last.load(file_name, path)
-	end
-	documents
+  documents = []
+  file_pattern = File.join(data_path, "*")
+  Dir[file_pattern].each do |string|
+    file_name = File.basename(string)
+    path = File.dirname(string)
+    documents << Document.new
+    documents.last.load(file_name, path)
+  end
+  documents
 end
 
 def find_by_name(filename)
-	same_names = @documents.reject do |document|
-		document.name.downcase != filename.downcase
-	end
-	same_names.first
+  same_names = @documents.reject do |document|
+    document.name.downcase != filename.downcase
+  end
+  same_names.first
 end
 
 #Return error if doc name has error, otherwise return nil
 def error_for_new_doc_name(filename, extension)
-	full_file_name = filename + "." + extension
+  full_file_name = filename + "." + extension
   if !(1..20).cover?(filename.size)
     return "File name must be between 1 and 20 characters"
   elsif find_by_name(full_file_name)
     return "File name must be unique"
   elsif (filename =~ /(^[A-Za-z][A-Za-z0-9_]+)$/).nil?
     return "Invalid file name.  File must begin with an alpha character.  The rest of the file name can only contain alphanumeric characters and underscores"
-	end
-	nil
+  end
+  nil
 end
 
 def signed_in?
-	session[:logged_in]
+  session[:logged_in]
 end
 
 def require_sign_in
-	if !signed_in?
-		session[:error] = "You must be signed in to do that"
-		redirect "/"
-	end
+  if !signed_in?
+    session[:error] = "You must be signed in to do that"
+    redirect "/"
+  end
 end
 
 def valid_login?(username, password)
-	return ACCOUNTS.has_key?(username) && BCrypt::Password.new(ACCOUNTS[username]) == password
+  return ACCOUNTS.has_key?(username) && BCrypt::Password.new(ACCOUNTS[username]) == password
 end
 
 get "/users/signin" do
-	erb :sign_in, layout: :layout
+  erb :sign_in, layout: :layout
 end
 
 post "/users/signin" do
-	@username = params[:username]
-	password = params[:password]
-	if valid_login?(@username,password)
-		session[:username] = @username
-		session[:logged_in] = true
-		session[:success] = "Welcome!"
-		redirect "/"
-	else
-		session[:error] = "Invalid Credentials"
-		@username = params[:username]
-		erb :sign_in, layout: :layout
-	end
+  @username = params[:username]
+  password = params[:password]
+  if valid_login?(@username,password)
+    session[:username] = @username
+    session[:logged_in] = true
+    session[:success] = "Welcome!"
+    redirect "/"
+  else
+    session[:error] = "Invalid Credentials"
+    @username = params[:username]
+    erb :sign_in, layout: :layout
+  end
 end
 
 #log user out
 post "/users/signout" do
-	session[:username] = nil
-	session[:logged_in] = false
-	session[:success] = "You have been signed out"
-	redirect "/"
+  session[:username] = nil
+  session[:logged_in] = false
+  session[:success] = "You have been signed out"
+  redirect "/"
 end
 
 get "/?" do
-	erb :index, layout: :layout
+  erb :index, layout: :layout
 end
 
 #View file content
 get "/:filename" do
-	document = find_by_name(params[:filename])
-	if document
-		headers["Content-Type"] = document.format_content[:headers][:content_type]
-		document.format_content[:body]
-	else
-		session[:error] = "#{params[:filename]} does not exist."
-		redirect '/'
-	end
+  document = find_by_name(params[:filename])
+  if document
+    headers["Content-Type"] = document.format_content[:headers][:content_type]
+    document.format_content[:body]
+  else
+    session[:error] = "#{params[:filename]} does not exist."
+    redirect '/'
+  end
 end
 
 #Render the edit file page
 get "/:filename/edit" do
-	require_sign_in
-	@document = find_by_name(params[:filename])
-	erb :edit_document
+  require_sign_in
+  @document = find_by_name(params[:filename])
+  erb :edit_document
 end
 
 # # Update/edit page information
 post "/:filename/edit" do
-	require_sign_in
-	@document = find_by_name(params[:filename])
+  require_sign_in
+  @document = find_by_name(params[:filename])
   @document.update_content(params[:file_content])
   session[:success] = "#{params[:filename]} has been updated."
   redirect "/"
@@ -143,42 +143,42 @@ end
 
 #Render the new file page
 get "/file/new" do
-	require_sign_in
-	@valid_file_types = Document::VALID_FILE_TYPES
-	erb :new_document, layout: :layout
+  require_sign_in
+  @valid_file_types = Document::VALID_FILE_TYPES
+  erb :new_document, layout: :layout
 end
 
 #Create new file page
 post "/file/new" do
-	require_sign_in
-	@valid_file_types = Document::VALID_FILE_TYPES
-	@file_name_base = params[:doc_name].strip
-	extension = params[:file_type]
+  require_sign_in
+  @valid_file_types = Document::VALID_FILE_TYPES
+  @file_name_base = params[:doc_name].strip
+  extension = params[:file_type]
 
-	error = error_for_new_doc_name(@file_name_base, extension)
-	
-	if error
-		session[:error] = error
-		status 422
-		@file_name_base
+  error = error_for_new_doc_name(@file_name_base, extension)
+  
+  if error
+    session[:error] = error
+    status 422
+    @file_name_base
     erb :new_document, layout: :layout
-	else
-		file_name = @file_name_base.strip + "." + extension		
-		@documents << Document.new
-		@documents.last.create_document(file_name, data_path)
-		session[:success] = "#{file_name} has been created."
-		redirect "/"
-	end
+  else
+    file_name = @file_name_base.strip + "." + extension   
+    @documents << Document.new
+    @documents.last.create_document(file_name, data_path)
+    session[:success] = "#{file_name} has been created."
+    redirect "/"
+  end
 end
 
 #Delete a file
 post "/:filename/delete" do
-	require_sign_in
-	filename = params[:filename]
-	doc = find_by_name(filename)
+  require_sign_in
+  filename = params[:filename]
+  doc = find_by_name(filename)
 
-	doc.delete_file
-	@documents.delete(doc)
-	session[:success] = "#{filename} was deleted"
-	redirect "/"
+  doc.delete_file
+  @documents.delete(doc)
+  session[:success] = "#{filename} was deleted"
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -11,181 +11,181 @@ require_relative "../cms"
 
 
 class CMSTest < Minitest::Test
-	include Rack::Test::Methods
+  include Rack::Test::Methods
 
-	def app
-		Sinatra::Application
-	end
+  def app
+    Sinatra::Application
+  end
 
-	def setup
-		FileUtils.mkdir_p(data_path)
-	end
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
 
-	def teardown
-		FileUtils.rm_rf(data_path)
-	end
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
 
-	def session
-		last_request.env["rack.session"]
-	end
+  def session
+    last_request.env["rack.session"]
+  end
 
-	def login
-		post "/users/signin", username: "admin", password: "test"
-	end
+  def login
+    post "/users/signin", username: "admin", password: "test"
+  end
 
-	def user_signed_in
-		{"rack.session" => { username: "admin", logged_in: true}}
-	end
+  def user_signed_in
+    {"rack.session" => { username: "admin", logged_in: true}}
+  end
 
-	def test_index
-		Document.new.create_document("about.txt", data_path)
-		Document.new.create_document("test.md", data_path)
-		get "/"
-		assert_equal 200, last_response.status
-		assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-		assert_includes last_response.body, "about.txt"
-		assert_includes last_response.body, "test.md"
-	end
+  def test_index
+    Document.new.create_document("about.txt", data_path)
+    Document.new.create_document("test.md", data_path)
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "test.md"
+  end
 
-	def test_view_about
-		doc = Document.new
-		doc.create_document("about.txt", data_path)
-		doc.update_content("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.")
-		get "/about.txt"
-		assert_equal 200, last_response.status
-		assert_equal "text/plain", last_response["Content-Type"]
-		assert_includes last_response.body, "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-	end
+  def test_view_about
+    doc = Document.new
+    doc.create_document("about.txt", data_path)
+    doc.update_content("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.")
+    get "/about.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  end
 
-	def test_view_nonexistant_document
-		get "/nonexistant.txt"
-		assert_equal 302, last_response.status
-		assert_equal "nonexistant.txt does not exist.", session[:error]
-	end
+  def test_view_nonexistant_document
+    get "/nonexistant.txt"
+    assert_equal 302, last_response.status
+    assert_equal "nonexistant.txt does not exist.", session[:error]
+  end
 
-	def test_view_markdown_document
-		test_content = "# Aristas posset
-			## Fit prodidit lateantque recessu proxima cum
-			Lorem markdownum illum scelerisque comes; constitit sine Phoebus hanc ab genus"
-		doc = Document.new
-		doc.create_document("test.md", data_path)
-		doc.update_content(test_content)
-		get "/test.md"
-		assert_equal 200, last_response.status
-		assert_equal "text/html", last_response["Content-Type"]
-		assert_includes last_response.body, "<h1>Aristas posset</h1>"
-	end
+  def test_view_markdown_document
+    test_content = "# Aristas posset
+      ## Fit prodidit lateantque recessu proxima cum
+      Lorem markdownum illum scelerisque comes; constitit sine Phoebus hanc ab genus"
+    doc = Document.new
+    doc.create_document("test.md", data_path)
+    doc.update_content(test_content)
+    get "/test.md"
+    assert_equal 200, last_response.status
+    assert_equal "text/html", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Aristas posset</h1>"
+  end
 
-	def test_edit_document_plaintext
-		Document.new.create_document("about.txt", data_path)	
-		get "/about.txt/edit",{}, user_signed_in
-		assert_equal 200, last_response.status
-	end
+  def test_edit_document_plaintext
+    Document.new.create_document("about.txt", data_path)  
+    get "/about.txt/edit",{}, user_signed_in
+    assert_equal 200, last_response.status
+  end
 
-	def test_edit_document_update_content
-		Document.new.create_document("trix.txt", data_path)	
-		post "/trix.txt/edit", {file_content: "Silly rabbit, trix are for kids"}, user_signed_in
+  def test_edit_document_update_content
+    Document.new.create_document("trix.txt", data_path) 
+    post "/trix.txt/edit", {file_content: "Silly rabbit, trix are for kids"}, user_signed_in
 
-		assert_equal 302, last_response.status
-		assert_equal "trix.txt has been updated.", session[:success]
-		
-		get "/trix.txt"
-		assert_equal 200, last_response.status
-		assert_equal "text/plain", last_response["Content-Type"]
-		assert_includes last_response.body, "Silly rabbit, trix are for kids"
-	end
+    assert_equal 302, last_response.status
+    assert_equal "trix.txt has been updated.", session[:success]
+    
+    get "/trix.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "Silly rabbit, trix are for kids"
+  end
 
-	def test_edit_document_update_logged_out
-		Document.new.create_document("trix.txt", data_path)	
-		post "/trix.txt/edit", {file_content: "Silly rabbit, trix are for kids"}
-		assert_equal "You must be signed in to do that", session[:error]
-		assert_equal 302, last_response.status
+  def test_edit_document_update_logged_out
+    Document.new.create_document("trix.txt", data_path) 
+    post "/trix.txt/edit", {file_content: "Silly rabbit, trix are for kids"}
+    assert_equal "You must be signed in to do that", session[:error]
+    assert_equal 302, last_response.status
 
-		get last_response["Location"]
-		assert_equal 200, last_response.status
-	end
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+  end
 
-	def test_new_document
-		get "/file/new", {}, user_signed_in
-		assert_equal 200, last_response.status
-		assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-		assert_includes last_response.body, "Create New Document"
-	end
+  def test_new_document
+    get "/file/new", {}, user_signed_in
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "Create New Document"
+  end
 
-	def test_new_document_create
-		post "file/new", {doc_name: "new_doc", file_type: "txt"}, user_signed_in
-		assert_equal 302, last_response.status
-		assert_equal "new_doc.txt has been created.", session[:success]
+  def test_new_document_create
+    post "file/new", {doc_name: "new_doc", file_type: "txt"}, user_signed_in
+    assert_equal 302, last_response.status
+    assert_equal "new_doc.txt has been created.", session[:success]
 
-		get last_response["Location"]
-		assert_equal 200, last_response.status
-		assert_includes last_response.body, "new_doc.txt"
-	end
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new_doc.txt"
+  end
 
-	def test_new_document_error
-		post "file/new", {doc_name: "", file_type: "txt"}, user_signed_in
-		assert_equal 422, last_response.status
-		assert_includes last_response.body, "File name must be between 1 and 20 characters"
-	end
+  def test_new_document_error
+    post "file/new", {doc_name: "", file_type: "txt"}, user_signed_in
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, "File name must be between 1 and 20 characters"
+  end
 
-	def test_new_document_create_logged_out
-		post "file/new", {doc_name: "new_doc", file_type: "txt"}
-		assert_equal "You must be signed in to do that", session[:error]
-		assert_equal 302, last_response.status
+  def test_new_document_create_logged_out
+    post "file/new", {doc_name: "new_doc", file_type: "txt"}
+    assert_equal "You must be signed in to do that", session[:error]
+    assert_equal 302, last_response.status
 
-		get last_response["Location"]
-		assert_equal 200, last_response.status
-	end
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+  end
 
-	def test_delete_document
-		Document.new.create_document("to_delete.txt", data_path)	
-		post "/to_delete.txt/delete", {}, user_signed_in
-		assert_equal 302, last_response.status
-		assert_equal "to_delete.txt was deleted", session[:success]
+  def test_delete_document
+    Document.new.create_document("to_delete.txt", data_path)  
+    post "/to_delete.txt/delete", {}, user_signed_in
+    assert_equal 302, last_response.status
+    assert_equal "to_delete.txt was deleted", session[:success]
 
-		get last_response["Location"]
-		assert_equal 200, last_response.status
-	end
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+  end
 
-	def test_delete_logged_out
-		Document.new.create_document("to_delete.txt", data_path)	
-		post "/to_delete.txt/delete"
-		assert_equal "You must be signed in to do that", session[:error]
-		assert_equal 302, last_response.status
+  def test_delete_logged_out
+    Document.new.create_document("to_delete.txt", data_path)  
+    post "/to_delete.txt/delete"
+    assert_equal "You must be signed in to do that", session[:error]
+    assert_equal 302, last_response.status
 
-		get last_response["Location"]
-		assert_equal 200, last_response.status
-	end
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+  end
 
-	def test_login_form
-		get '/users/signin'
-		assert_equal 200, last_response.status
+  def test_login_form
+    get '/users/signin'
+    assert_equal 200, last_response.status
 
-		assert_includes last_response.body, "Username"
-		assert_includes last_response.body, "Password"
-		assert_includes last_response.body, "Sign In"
-	end
+    assert_includes last_response.body, "Username"
+    assert_includes last_response.body, "Password"
+    assert_includes last_response.body, "Sign In"
+  end
 
-	def test_login
-		post '/users/signin', {username: "admin", password: "test"}
-		assert_equal "Welcome!", session[:success]
-		assert_equal true, session[:logged_in]
-		assert_equal "admin", session[:username]
-		assert_equal 302, last_response.status
+  def test_login
+    post '/users/signin', {username: "admin", password: "test"}
+    assert_equal "Welcome!", session[:success]
+    assert_equal true, session[:logged_in]
+    assert_equal "admin", session[:username]
+    assert_equal 302, last_response.status
 
-		get last_response["Location"]
-		assert_equal 200, last_response.status
-	end
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+  end
 
-	def test_login_invalid
-		post '/users/signin', {username: "wrong", password: "bad"}
-		assert_includes last_response.body, "Invalid Credentials"
-	end
+  def test_login_invalid
+    post '/users/signin', {username: "wrong", password: "bad"}
+    assert_includes last_response.body, "Invalid Credentials"
+  end
 
-	def test_logout
-		post 'users/signout'
-		assert_equal false, session[:logged_in]
-		assert_equal nil, session[:username]
-		assert_equal "You have been signed out", session[:success]
-	end
+  def test_logout
+    post 'users/signout'
+    assert_equal false, session[:logged_in]
+    assert_equal nil, session[:username]
+    assert_equal "You have been signed out", session[:success]
+  end
 end
